c61cbe6353951ba94e4db903dcdc43a0821bcd44
Yeray Expósito <alu0100951844@ull.edu.es>
2017-10-24 19:05:35 +0000

    C8:Crear variables de instancia en el initialize y almacenar los datos

--- lib/Alimentos.rb
+++ lib/Alimentos.rb
@@ -1,7 +1,11 @@
 
 class Alimentos
 	
-	def initialize
+	def initialize(datos_alimento)
+	   @nombre = datos_alimento[0]
+       @proteinas = datos_alimento[1]
+       @glucidos = datos_alimento[2]
+       @lipidos = datos_alimento[3]
     end
-	
+    
 end
