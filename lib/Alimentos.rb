f56d5b3f0ad6228a019c65f26106919a25667a5d
Yeray Expósito <alu0100951844@ull.edu.es>
2017-10-24 20:14:26 +0000

    C21:Crear método para calcular el valor energético

--- lib/Alimentos.rb
+++ lib/Alimentos.rb
@@ -1,17 +1,21 @@
 
 class Alimentos
 	
-	attr_reader :nombre, :proteinas, :glucidos, :lipidos
+   attr_reader :nombre, :proteinas, :glucidos, :lipidos
 	
-	def initialize(datos_alimento)
-	   @nombre = datos_alimento[0]
-	   @proteinas = datos_alimento[1]
-       @glucidos = datos_alimento[2]
-       @lipidos = datos_alimento[3]
-    end
-    
-    def to_s
+   def initialize(datos_alimento)
+      @nombre = datos_alimento[0]
+      @proteinas = datos_alimento[1]
+      @glucidos = datos_alimento[2]
+      @lipidos = datos_alimento[3]
+   end
+   
+   def to_s
       "Nombre del alimento: #{@nombre}  Proteínas: #{@proteinas}g  Glúcidos: #{@glucidos}g  Lípidos: #{@lipidos}g"
-    end
-    
+   end
+   
+   def valor_energetico
+      @glucidos*4 + @lipidos*9 + @proteinas*4
+   end
+   
 end
