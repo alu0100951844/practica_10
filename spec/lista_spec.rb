ab4a36081a12229d682960f47ca55d11daff8ca5
Yeray Expósito <alu0100951844@ull.edu.es>
2017-11-09 19:25:26 +0000

    C36: ERROR==> No existe método extract_head!

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -8,6 +8,11 @@ describe List do
        @lista_2 = List.new()
        @lista_2.insert_tail!(3)
        @lista_2.insert_head!(5)
+       
+       @lista_3 = List.new()
+       @lista_3.insert_tail!(7)
+       @lista_3.insert_head!(8)
+       @lista_3.extract_head!
       
     end
     
@@ -38,6 +43,20 @@ describe List do
         
             it "El tamaño de la lista es correcto" do
                 expect(@lista_2.sz).to eq(2)
+            end
+        end
+        
+        context "Si se establece cola y cabeza en la lista pero se quita la cabeza despues" do    
+            it "La cola de la lista es correcta" do
+                expect(@lista_3.tail.value).to eq(7)
+            end
+        
+            it "La cabeza de la lista es correcta" do
+                expect(@lista_3.head.value).to eq(7)
+            end
+        
+            it "El tamaño de la lista es correcto" do
+                expect(@lista_3.sz).to eq(1)
             end
         end
     end
