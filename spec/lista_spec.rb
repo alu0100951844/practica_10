68951c9b4c31dda3c7c7c1811bb23c9d6e9a1c06
Yeray Expósito <alu0100951844@ull.edu.es>
2017-11-09 19:02:25 +0000

    C30: ERROR==> No existen variables de instancia

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -4,6 +4,24 @@ describe List do
 	
 	before :each do
        @lista = List.new()
+      
+    end
+    
+    describe "# datos de una lista" do
+        
+        context "Si no se establece cola ni cabeza en la lista" do    
+            it "Cola de la lista por defecto" do
+                expect(@lista.tail).to eq(nil)
+            end
+        
+            it "Cabeza de la lista por defecto" do
+                expect(@lista.head).to eq(nil)
+            end
+        
+            it "Tamaño de la lista por defecto" do
+                expect(@lista.sz).to eq(0)
+            end
+        end
     end
    
 end
