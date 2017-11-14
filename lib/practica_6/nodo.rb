103cdbbd4cf75a3ae596c03bf0d1c1511ec59968
Yeray Expósito <alu0100951844@ull.edu.es>
2017-11-14 20:14:33 +0000

    85C: Comentando clase Node

--- lib/practica_6/nodo.rb
+++ lib/practica_6/nodo.rb
@@ -1,2 +1,4 @@
 
+# @author Yeray Expósito García
+# La clase mutable Node (generada con Struct) representa la estructura básica para la implementación de la lista doblemente enlazada.
 Node = Struct.new(:value, :next, :prev)
